179118321cbb754c1f5430f6831d410e9e00c839
David Seddon <david.seddon@growthstreet.co.uk>
2016-11-26 13:22:11 +0000

    Starting to prepare site

--- project/main.py
+++ project/main.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+
 import functools
 import os.path
 import re
@@ -22,10 +9,6 @@ import tornado.wsgi
 import unicodedata
 
 from google.appengine.api import users
-from google.appengine.ext import db
-
-import webapp2
-
 
 
 def administrator(method):
@@ -66,104 +49,63 @@ class BaseHandler(tornado.web.RequestHandler):
 
 class HomeHandler(BaseHandler):
     def get(self):
-        entries = db.Query(Entry).order('-published').fetch(limit=5)
-        if not entries:
-            if not self.current_user or self.current_user.administrator:
-                self.redirect("/compose")
-                return
-        self.render("home.html", entries=entries)
+        self.render("home.html")
 
 
-class EntryHandler(BaseHandler):
-    def get(self, slug):
-        entry = db.Query(Entry).filter("slug =", slug).get()
-        if not entry: raise tornado.web.HTTPError(404)
-        self.render("entry.html", entry=entry)
-
-
-class ArchiveHandler(BaseHandler):
-    def get(self):
-        entries = db.Query(Entry).order('-published')
-        self.render("archive.html", entries=entries)
-
-
-class FeedHandler(BaseHandler):
-    def get(self):
-        entries = db.Query(Entry).order('-published').fetch(limit=10)
-        self.set_header("Content-Type", "application/atom+xml")
-        self.render("feed.xml", entries=entries)
-
-
-class FooHandler(BaseHandler):
-    def get(self):
-        """Simple request handler that shows all of the MySQL variables."""
-        db = connect_to_cloudsql()
-        cursor = db.cursor()
-        cursor.execute('SHOW VARIABLES')
-
-        variables = []
-        for r in cursor.fetchall():
-            variables.append('{}'.format(r))
-        self.render("foo.html", variables=variables)
-
-
-class ComposeHandler(BaseHandler):
+class AdminHandler(BaseHandler):
     @administrator
     def get(self):
-        key = self.get_argument("key", None)
-        entry = Entry.get(key) if key else None
-        self.render("compose.html", entry=entry)
-
-    @administrator
-    def post(self):
-        key = self.get_argument("key", None)
-        if key:
-            entry = Entry.get(key)
-            entry.title = self.get_argument("title")
-            entry.body_source = self.get_argument("body_source")
-            entry.html = tornado.escape.linkify(
-                self.get_argument("body_source"))
-        else:
-            title = self.get_argument("title")
-            slug = unicodedata.normalize("NFKD", title).encode(
-                "ascii", "ignore")
-            slug = re.sub(r"[^\w]+", " ", slug)
-            slug = "-".join(slug.lower().strip().split())
-            if not slug: slug = "entry"
-            while True:
-                existing = db.Query(Entry).filter("slug =", slug).get()
-                if not existing or str(existing.key()) == key:
-                    break
-                slug += "-2"
-            entry = Entry(
-                author=self.current_user,
-                title=title,
-                slug=slug,
-                body_source=self.get_argument("body_source"),
-                html=tornado.escape.linkify(self.get_argument("body_source")),
-            )
-        entry.put()
-        self.redirect("/entry/" + entry.slug)
+        self.render("admin.html", words=[])
 
 
-class EntryModule(tornado.web.UIModule):
-    def render(self, entry):
-        return self.render_string("modules/entry.html", entry=entry)
+# class ComposeHandler(BaseHandler):
+#     @administrator
+#     def get(self):
+#         key = self.get_argument("key", None)
+#         entry = Entry.get(key) if key else None
+#         self.render("compose.html", entry=entry)
+# 
+#     @administrator
+#     def post(self):
+#         key = self.get_argument("key", None)
+#         if key:
+#             entry = Entry.get(key)
+#             entry.title = self.get_argument("title")
+#             entry.body_source = self.get_argument("body_source")
+#             entry.html = tornado.escape.linkify(
+#                 self.get_argument("body_source"))
+#         else:
+#             title = self.get_argument("title")
+#             slug = unicodedata.normalize("NFKD", title).encode(
+#                 "ascii", "ignore")
+#             slug = re.sub(r"[^\w]+", " ", slug)
+#             slug = "-".join(slug.lower().strip().split())
+#             if not slug: slug = "entry"
+#             while True:
+#                 existing = db.Query(Entry).filter("slug =", slug).get()
+#                 if not existing or str(existing.key()) == key:
+#                     break
+#                 slug += "-2"
+#             entry = Entry(
+#                 author=self.current_user,
+#                 title=title,
+#                 slug=slug,
+#                 body_source=self.get_argument("body_source"),
+#                 html=tornado.escape.linkify(self.get_argument("body_source")),
+#             )
+#         entry.put()
+#         self.redirect("/entry/" + entry.slug)
 
 
 settings = {
-    "blog_title": u"Tornado Blog",
-    "template_path": os.path.join(os.path.dirname(__file__), "templates"),
-    "ui_modules": {"Entry": EntryModule},
+    "site_title": u"Cumulus",
+    "template_path": os.path.join(os.path.dirname(__file__), "..", "templates"),
+    # "ui_modules": {"Entry": EntryModule},
     "xsrf_cookies": True,
 }
 application = tornado.web.Application([
     (r"/", HomeHandler),
-    (r"/archive", ArchiveHandler),
-    (r"/feed", FeedHandler),
-    (r"/entry/([^/]+)", EntryHandler),
-    (r"/compose", ComposeHandler),
-    (r"/foo", FooHandler),
+    (r"/admin", AdminHandler),
 ], **settings)
 
 application = tornado.wsgi.WSGIAdapter(application)
