724889479aec0a81774ed483140cca43c236015d
David Seddon <david.seddon@growthstreet.co.uk>
2016-11-26 11:35:44 +0000

    Testing mysql connection

--- blog.py
+++ blog.py
@@ -26,6 +26,44 @@ from google.appengine.api import users
 from google.appengine.ext import db
 
 
+import os
+
+import MySQLdb
+import webapp2
+
+
+# These environment variables are configured in app.yaml.
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+
+def connect_to_cloudsql():
+    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+    # will be set to 'Google App Engine/version'.
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD)
+
+    # If the unix socket is unavailable, then try to connect using TCP. This
+    # will work if you're running a local MySQL server or using the Cloud SQL
+    # proxy, for example:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    else:
+        db = MySQLdb.connect(
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+
+    return db
+
+
 class Entry(db.Model):
     """A single blog entry."""
     author = db.UserProperty()
@@ -103,6 +141,19 @@ class FeedHandler(BaseHandler):
         self.render("feed.xml", entries=entries)
 
 
+class FooHandler(BaseHandler):
+    def get(self):
+        """Simple request handler that shows all of the MySQL variables."""
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        cursor.execute('SHOW VARIABLES')
+
+        variables = []
+        for r in cursor.fetchall():
+            variables.append('{}'.format(r))
+        self.render("foo.html", variables=variables)
+
+
 class ComposeHandler(BaseHandler):
     @administrator
     def get(self):
@@ -159,6 +210,7 @@ application = tornado.web.Application([
     (r"/feed", FeedHandler),
     (r"/entry/([^/]+)", EntryHandler),
     (r"/compose", ComposeHandler),
+    (r"/foo", FooHandler),
 ], **settings)
 
 application = tornado.wsgi.WSGIAdapter(application)
